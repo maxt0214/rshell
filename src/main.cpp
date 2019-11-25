1309b72952e5ac416e8374811787d5932ff42f73
maxt0214 <49177628+maxt0214@users.noreply.github.com>
2019-11-25 00:51:18 -0800

    Updated main.cpp
    Modified the main.cpp to better work with new added feactures
    Free memories allocated for char**

--- src/main.cpp
+++ src/main.cpp
@@ -11,62 +11,151 @@
 
 using namespace std;
 
+Base* GenerateSingleCommand(char** currCommand, int& currLoc, int end);
+Base* GenerateCommands(vector<char**> commands, int start, int end); //generate either test, command, or exit
+int searchParenthesis(vector<char**> commands, unsigned start);
+
 int main() {
     string userInput;
-    vector<char**> commands;
-    char exitString[7] = "exit";
-    char semicolonString[2] = ";";
-    char andString[3] = "&&";
-    char orString[3] = "||";
     
     do {
+        //prompt the user and get the input
         cout << "$ ";
-        getline(cin,userInput);
-        cout << endl;
+        getline(cin, userInput);
+        //set up the parsed commands
+        parser parsedInput(userInput);
+        vector<char**> commands = parsedInput.parse();
         
-        if(userInput.size() != 0) {
-        
-            //check if sytax error
-            unsigned counter = 0;
-            while(userInput.at(counter) == ' ') {
-                counter ++;
-            }
-            
-            if(userInput.at(counter) == ';' || userInput.at(counter) == '&' || userInput.at(counter) == '|' || userInput.at(counter) == '#') {
-                cout << "Syntax error!" << endl;
+        //check if no commands input by user
+        if(commands.size() != 0) {
+            //Set up the commands and execute
+            Base* executableCommand = GenerateCommands(commands, 0, commands.size()-1);
+            if(executableCommand == NULL) {
+                cout << "Syntax Error!" << endl;
             } else {
-                parser parsedcommand(userInput);
-                commands = parsedcommand.parse();
+                executableCommand->Execute();
+            }
+            //free memories
+            for(unsigned i = 0; i < commands.size(); i++) {
+                delete[] commands.at(i);
             }
         }
-        
-        if(commands.size() == 1) {//single command
-            Exit* curr = new Exit(commands.at(0));
-            curr->Execute();
-        }
-        
-        if(commands.size() > 1) {//multiple commands
-            Base* last = new Exit(commands.at(0));
-            for(unsigned i = 1; i < commands.size() - 1; i++) {
-                
-                if(*commands.at(i) == semicolonString) {
-                    SEMI* semiConnector = new SEMI(last,new Exit(commands.at(i+1)));
-                    last = semiConnector;
-                }
-                
-                if(*commands.at(i)[0] == *andString) {
-                    AND* andConnector = new AND(last,new Exit(commands.at(i+1)));
-                    last = andConnector;
-                }
-                
-                if(*commands.at(i)[0] == *orString) {
-                    OR* orConnector = new OR(last,new Exit(commands.at(i+1)));
-                    last = orConnector;
-                }
-            }
-            
-            last->Execute();
-        }
-        
     } while(!cin.fail());
+}
+
+Base* GenerateSingleCommand(vector<char**> commands, int& currLoc, int end) {
+    Base* curr;
+    char** currCommand = commands.at(currLoc);
+    string temp = currCommand[0];
+    
+    if(temp == "(") {//search for ) to continue
+        int final = searchParenthesis(commands, currLoc);
+        if(final == 0) {//error
+            return NULL;
+        } else {
+            curr = GenerateCommands(commands,currLoc+1,final-1);
+            if(curr == NULL) {//syntax error in 
+                return NULL;
+            }
+        }
+        //update currLoc
+        currLoc = final;
+    } else if(temp == "[") {//search for ] to continue
+        if(currLoc + 2 <= end) {//still have at least two more command behind
+            
+            if(currCommand)
+        } else if(currLoc + 1 <= end) {
+            
+        } else {
+            
+        }
+    } else if(temp == "]" && temp == ")" || temp == "&&" || temp == "||" || temp == ";") {//syntax error
+        return NULL;
+    } else if(temp == "test") {
+        
+        if(currLoc + 1 > end) {//only contains a "test"
+            
+        } else {//have something other than "test"
+            currLoc ++; //discard test
+            currCommand = commands.at(currLoc);
+            temp = currCommand[0];
+            
+            if(temp == "&&" || temp == "||" || temp == ";") {// input is: test && || ; ...
+                
+            } else {// input is: test ...
+                
+            }
+        }
+        
+    } else if(temp == "exit") {
+        curr = new Exit(currCommand);
+    } else {//normal command
+        curr = new Command(currCommand);
+    }
+    //update currLoc
+    currLoc ++;
+    
+    return curr;
+}
+
+Base* GenerateCommands(vector<char**> commands, int start, int end) {
+    Base* lf,* mid,* rt;
+    int currLoc = start;
+    
+    lf = GenerateSingleCommand(commands, currLoc, end);
+    
+    while(currLoc <= end) {//if executed, currLoc now should point to the middle connector
+        
+        if(currLoc + 1 > end) {//check if there is a right command
+            return NULL;
+        }
+        
+        char** currCommand = commands.at(currLoc);
+        string temp = currCommand[0];
+        //mid connector
+        if(temp == "(" || temp == ")" || temp == "[" || temp == "]") {//error, should only be a connector
+            return NULL;
+        } else if(temp == "&&") {
+            currLoc ++;
+            rt = GenerateSingleCommand(commands, currLoc, end);
+            mid = new AND(lf,rt);
+        } else if(temp == "||") {
+            currLoc ++;
+            rt = GenerateSingleCommand(commands, currLoc, end);
+            mid = new OR(lf,rt);
+        } else if(temp == ";") {
+            currLoc ++;
+            rt = GenerateSingleCommand(commands, currLoc, end);
+            mid = new SEMI(lf,rt);
+        } else {//the middle connector is test, error
+            return NULL;
+        }
+        
+        lf = mid;
+    }
+    
+    return lf;
+}
+
+int searchParenthesis(vector<char**> commands, unsigned start) {
+    char** currCommand;
+    string ifparenthesis;
+    int additionalParenthesis = 0, MatchLoc = 0;
+    for(int i = start+1; i < commands.size(); i ++) {
+        currCommand = commands.at(i);
+        ifparenthesis = currCommand[0];
+        //check for additional ( on the way searching for )
+        if(ifparenthesis == "(") {
+            additionalParenthesis ++;
+        }
+        if(ifparenthesis == ")") {
+            if(additionalParenthesis > 0) { //skip for the )'s matching for other (
+                additionalParenthesis--;
+            } else {
+                return i;
+            }
+        }
+    }
+    //0 means not found, otherwise found
+    return MatchLoc;
 }
